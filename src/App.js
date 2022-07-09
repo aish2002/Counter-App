f2378be2bacde0c46c5a876f6573fd19729b226b
unknown <aishwaryaaj2002@gmail.com>
2022-07-09 14:03:51 +0530

    Created Counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { Card,Button,Row,Col} from 'react-bootstrap';
 
 function App() {
+  const [val, setval] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className=" min-vh-100 justify-content-center align-items-center counter">
+      
+    <Row className="justify-content-center align-items-center   ">
+    <Col md="4" sm="6" xs="6" className="column1" > 
+        <h1 className="text-center mt-5 pt-5">Counter</h1>
+            <Card > 
+             <Card.Body> {val}</Card.Body> </Card>
+            <Row className="justify-content-center align-items-center ml-5 mr-5 ">
+            <Button  md="4" xs="6" variant="primary"  className="mt-3  inc"
+             onClick={()=>setval(val+1)} block>
+              Increment +
+            </Button>
+            <br/>
+            <Button  md="6" sm="" xs="6" variant="primary"  className="mt-3  dec" 
+            onClick={()=>setval(val-1)} block>
+              Decrement -
+            </Button>
+            </Row>
+          </Col>
+      </Row>
+      </div>
   );
 }
 
 export default App;
+
